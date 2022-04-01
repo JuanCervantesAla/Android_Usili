459d4e209159711301154114c9658cce4392f56e
EmilianoCer <emijaslayer@gmail.com>
2022-03-31 20:20:19 -0600

    5th save 31/03/2022

--- app/src/main/java/com/example/usiliv101/Agregar.java
+++ app/src/main/java/com/example/usiliv101/Agregar.java
@@ -1,17 +1,24 @@
 package com.example.usiliv101;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,6 +27,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Random;
 import java.util.UUID;
@@ -30,15 +41,25 @@ public class Agregar extends AppCompatActivity {
     Button btnVolver_Agregar,btnContinuar_Agregar;
     EditText edtETitulo_Agregar,edtEMateriales_Agregar,edtEPasos_Agregar;
     TextView txtEscondido_Agregar;
+    ImageView imgVImagen_Agregar;
+    public Uri imagenUri;
 
     private DatabaseReference reference;//Ruta para la tabla Usuarios
     private String idUsuario;
     private FirebaseUser usuario;//Autenticación del usuario, obtengo al usuario en sesion
+    private FirebaseStorage storage;//Atributo contenedor de referencia al almacenamiento
+    private StorageReference storageReference;//Atributo de contenedor de referencia al almacenamiento
+    private DatabaseReference reference2;//Ruta para la tabla perfiles, se guarda la fecha y la URL vease linea 230
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
+
+        //Subida de foto
+        storage =  FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        reference2= FirebaseDatabase.getInstance().getReference("Apoyo");//Tomo la referencia de mi base de datos root para subir fecha y url en tabla perfiles
 
         //Relacion JAVA - XML
         btnVolver_Agregar = findViewById(R.id.btnVolver_Agregar);
@@ -47,6 +68,7 @@ public class Agregar extends AppCompatActivity {
         edtEMateriales_Agregar  =findViewById(R.id.edtEMateriales_Agregar);
         edtEPasos_Agregar = findViewById(R.id.edtEPasos_Agregar);
         txtEscondido_Agregar = findViewById(R.id.txtEscondido_Agregar);
+        imgVImagen_Agregar = findViewById(R.id.imgVImagen_Agregar);
 
 
         //Boton para volver a la activity front
@@ -65,9 +87,94 @@ public class Agregar extends AppCompatActivity {
             public void onClick(View view) {
 
                 subirDatos();
-
             }
         });
+
+        imgVImagen_Agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                escogerFoto();
+            }
+        });
+
+    }
+
+    private void escogerFoto() {
+        //Creo la intencion
+        Intent intent = new Intent();
+        //Defino de que tipo es la intencion y el tipo de imagenes a guardar, el /* indica que todo tipo de imagenes
+        intent.setType("image/*");
+        //Declaro el tipo de accion para la intencion, es decir deseo traer datos
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        //Inicio la actividad para retraer info, parecido a cuando abro la camara, solo que abre la aplicacion de archivos por defecto
+        startActivityForResult(intent,1);//Guardo el numero de request para identificar que accion se hizo
+    }
+
+    //Cuando se realizo la apertura de la app de archivos y se escogió una foto... en resultado se hace....
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Si el codigo de resultado es 1 como en la linea 210 indiqué, y el resultado es un ok y si el dato que se selecciono no esta vacio(seleccione una imagen)
+        if(requestCode == 1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            //Obten la ruta del fichero de la imagen la cual seleccione
+            imagenUri = data.getData();
+            //Setea en el imageView la imagen que selecciones desde su ruta apuntada
+            imgVImagen_Agregar.setImageURI(imagenUri);
+            //Creo el metodo subir foto para subida a BD
+//            subirFoto(imagenUri);
+        }
+    }
+
+    //************METODO PARA SUBIDA DE FOTO
+    private void subirFoto(Uri uri,int id) {
+        //Creo un cuadro de dialogo parecido a un JOptionPane en Java normal
+        final ProgressDialog pd = new ProgressDialog(this);
+        //Asigno un titulo al dialogo
+        pd.setTitle("Subiendo imagen...");
+        //Muestro el cuadro de dialogo
+        pd.show();
+
+
+        final String randomkey  = UUID.randomUUID().toString();//Creo una llave random para almacenar las imagenes en una carpeta
+        //Obtengo la referencia de la imagen
+        final StorageReference riversRef = storageReference.child("articulos/"+idUsuario+"/"+randomkey);
+        //Obtengo la referencia de mi almacenamiento guardado en la rama(child) images/(carpeta) dentro de la carpeta del Id del usuario y
+        // asu vez quiero que la imagen se llame profile.jpeg esto permite que no se repita mas imagenes de perfil y todas las imagenes subidas se reemplazen por una nueva
+        riversRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {//Guardo la ruta de la imagen en la rama, la ruta que obtuvimos en el Activity OnResult
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                riversRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {//obtengo de la referencia la Url con la cual se sube el archivo
+                    @Override
+                    public void onSuccess(Uri uri) {//Si esto es exitoso
+
+                        Perfiles p = new Perfiles();//Creo un objeto p de las clase perfiles
+                        String G = uri.toString();//Ingreso en un atributo la url subida en String
+                        String pId = reference2.push().getKey();//Obtengo la llave de referencia con la cual se sube los datos a la BD
+                        //Ambos datos se guardarian en la llave obtenida algo como Woa743sdFDwas/Url y Woa743sdFDwas/Fecha por decir un ejemplo
+                        reference2.child(pId).child("Url").setValue(G);//Subo en la rama de la llave un child que diga url
+                        reference2.child(pId).child("Id").setValue(id);//Subo en la rama de la llave un child que diga url
+                        Glide.with(getApplicationContext()).load(uri).into(imgVImagen_Agregar);
+                        //reference2.child(pId).child("IdUsuario").setValue(idUsuario);
+                        pd.dismiss();//Cierro el cuadro de dialogo abierto de subiendo imagen
+                        Toast.makeText(Agregar.this, "Se subió correctamente", Toast.LENGTH_SHORT).show();//Muestro un mensaje de que se subió correctamente
+
+                    }
+                });
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {//Mientras se hace el meotod Succes
+                double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());//Obten los milisegundos en que tarda la imagen en cargar
+                pd.setMessage("Progress: "+(int)progressPercent + "%");//Conviertelo a un porcentaje del 0 al 100 porciento y muestralo en el caudro de dialogo
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {//Si falla
+                pd.dismiss();//Cierra el cuadro de dialogo
+                Toast.makeText(getApplicationContext(),"No se pudo subir la imagen",Toast.LENGTH_LONG).show();//Muestra un mensaje de eror
+            }
+        });
+
 
     }
 
@@ -77,6 +184,7 @@ public class Agregar extends AppCompatActivity {
         String materiales = edtEMateriales_Agregar.getText().toString().trim();
         String pasos = edtEPasos_Agregar.getText().toString().trim();
         String autor = "Hola";
+        String enlace = "";
         final String randomkey  = UUID.randomUUID().toString();//Creo una llave random para almacenar las imagenes en una carpeta
         final int min = 1000;
         final int max = 2000;
@@ -105,8 +213,9 @@ public class Agregar extends AppCompatActivity {
             edtEPasos_Agregar.requestFocus();
             return;
         }
+         subirFoto(imagenUri,id);
 
-        Articulos articulo =  new Articulos(titulo,pasos,materiales,autor,id);
+        Articulos articulo =  new Articulos(titulo,pasos,materiales,autor,id,enlace);
         FirebaseDatabase.getInstance().getReference("Articulos")
                 .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(randomkey).setValue(articulo).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
--- app/src/main/java/com/example/usiliv101/Articulos.java
+++ app/src/main/java/com/example/usiliv101/Articulos.java
@@ -2,16 +2,25 @@ package com.example.usiliv101;
 
 public class Articulos {
 
-    public String titulo, pasos, materiales, autor;
+    public String titulo, pasos, materiales, autor, enlace;
     public Integer id;
 
     public Articulos(){}
-    public Articulos(String titulo, String pasos, String materiales, String autor, Integer id) {
+    public Articulos(String titulo, String pasos, String materiales, String autor, Integer id,String enalce) {
         this.titulo = titulo;
         this.pasos = pasos;
         this.materiales = materiales;
         this.autor=autor;
         this.id = id;
+        this.enlace=enlace;
+    }
+
+    public String getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(String enlace) {
+        this.enlace = enlace;
     }
 
     public Integer getId() {
